7db59f72ff34a50ae49d1a433c26d3489a8a86c5
RenatTest <renattesttask@gmail.com>
2022-08-08 17:06:27 +0300

    check items quantity

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyListPage extends StatelessWidget {
+  final items = 15;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,37 +29,43 @@ class MyListPage extends StatelessWidget {
               'assets/logo.png',
             ),
           ),
-          Expanded(
-            child: RawScrollbar(
-              radius: Radius.circular(5),
-              thumbColor: Colors.black,
-              thickness: 5,
-              isAlwaysShown: true,
-              child: GridView.count(
-                physics: BouncingScrollPhysics(),
-                childAspectRatio: 2.0,
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                ),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 2,
-                children: List.generate(15, (index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 5)),
-                    child: Center(
-                      child: Text(
-                        'Item ${index + 1}',
-                        style: TextStyle(fontSize: 18.0),
+          items == 0
+              ? Text(
+                  'No items',
+                  style: TextStyle(fontSize: 18.0),
+                )
+              : Expanded(
+                  child: RawScrollbar(
+                    radius: Radius.circular(5),
+                    thumbColor: Colors.black,
+                    thickness: 5,
+                    isAlwaysShown: true,
+                    child: GridView.count(
+                      physics: BouncingScrollPhysics(),
+                      childAspectRatio: 2.0,
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        right: 10,
                       ),
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisCount: 2,
+                      children: List.generate(items, (index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 5)),
+                          child: Center(
+                            child: Text(
+                              'Item ${index + 1}',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                        );
+                      }),
                     ),
-                  );
-                }),
-              ),
-            ),
-          )
+                  ),
+                )
         ],
       ),
     );
