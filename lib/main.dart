848be0b9273c89793d1f921ae19de68037b276f4
RenatTest <renattesttask@gmail.com>
2022-08-08 18:25:36 +0300

    added SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -15,58 +15,60 @@ class MyApp extends StatelessWidget {
 }
 
 class MyListPage extends StatelessWidget {
-  final items = 15;
+  final items = 5;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.all(30.0),
-            child: Image.asset(
-              'assets/logo.png',
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(30.0),
+              child: Image.asset(
+                'assets/logo.png',
+              ),
             ),
-          ),
-          items == 0
-              ? Text(
-                  'No items',
-                  style: TextStyle(fontSize: 18.0),
-                )
-              : Expanded(
-                  child: RawScrollbar(
-                    radius: Radius.circular(5),
-                    thumbColor: Colors.black,
-                    thickness: 5,
-                    isAlwaysShown: true,
-                    child: GridView.count(
-                      physics: BouncingScrollPhysics(),
-                      childAspectRatio: 2.0,
-                      padding: const EdgeInsets.only(
-                        left: 10,
-                        right: 10,
-                      ),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 2,
-                      children: List.generate(items, (index) {
-                        return Container(
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.black, width: 5)),
-                          child: Center(
-                            child: Text(
-                              'Item ${index + 1}',
-                              style: TextStyle(fontSize: 18.0),
+            items == 0
+                ? Text(
+                    'No items',
+                    style: TextStyle(fontSize: 18.0),
+                  )
+                : Expanded(
+                    child: RawScrollbar(
+                      radius: Radius.circular(5),
+                      thumbColor: Colors.black,
+                      thickness: 5,
+                      isAlwaysShown: true,
+                      child: GridView.count(
+                        physics: BouncingScrollPhysics(),
+                        childAspectRatio: 2.0,
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          right: 10,
+                        ),
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        crossAxisCount: 2,
+                        children: List.generate(items, (index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.black, width: 5)),
+                            child: Center(
+                              child: Text(
+                                'Item ${index + 1}',
+                                style: TextStyle(fontSize: 18.0),
+                              ),
                             ),
-                          ),
-                        );
-                      }),
+                          );
+                        }),
+                      ),
                     ),
-                  ),
-                )
-        ],
+                  )
+          ],
+        ),
       ),
     );
   }
